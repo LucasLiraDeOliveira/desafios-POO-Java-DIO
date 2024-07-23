690e1d502cfdfd96ded17a128e536b2593439af4
LucasLiraDeOliveira <lucas.oliveira845@gmail.com>
2024-07-23 13:14:48 -0300

    Finished v1.0 of Main.java

--- desafio-dio-banco/src/defaultPackage/Main.java
+++ desafio-dio-banco/src/defaultPackage/Main.java
@@ -2,6 +2,24 @@ package defaultPackage;
 
 public class Main {
     public static void main(String[] args) {
+        //Criando clientes:
+        Cliente lucas = new Cliente("Lucas");
+        Cliente pedro = new Cliente("Pedro");
 
+        //Criando as contas
+        Conta cc = new ContaCorrente(lucas);
+        Conta cp = new ContaPoupanca(pedro);
+
+        //Atribuindo valores a elas via deposito
+        cc.depositar(120);
+        cp.depositar(100);
+
+        cc.imprimirExtrato();
+        cp.imprimirExtrato();
+
+        //Transferindo valores entre contas
+        cc.transferir(80, cp);
+        cc.imprimirExtrato();
+        cp.imprimirExtrato();
     }
 }
