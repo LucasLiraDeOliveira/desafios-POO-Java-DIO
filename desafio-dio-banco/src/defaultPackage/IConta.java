881fc35a81120f1325890d68f2e7a6396e27d0a4
LucasLiraDeOliveira <lucas.oliveira845@gmail.com>
2024-07-23 13:15:14 -0300

    Finished v1.0 of IConta.java

--- desafio-dio-banco/src/defaultPackage/IConta.java
+++ desafio-dio-banco/src/defaultPackage/IConta.java
@@ -1,4 +1,10 @@
 package defaultPackage;
 
 public interface IConta {
+    void sacar(double valor);
+    void depositar(double valor);
+    void transferir(double valor, IConta contaDestino); // em vez de por a interface IConta
+    // poderia ser a classe pai Conta
+
+    void imprimirExtrato();
 }
