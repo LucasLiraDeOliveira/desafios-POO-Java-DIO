4757ecae30020c684dac78635f28f9f28099afec
LucasLiraDeOliveira <lucas.oliveira845@gmail.com>
2024-07-24 10:56:16 -0300

    Instanciate the objects and assigned values to them in Main

--- desafio-dio-bootcampSimulator/src/Main.java
+++ desafio-dio-bootcampSimulator/src/Main.java
@@ -1,6 +1,33 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        //Vamos instanciar(criar) variaveis do tipo Curso e Mentoria
+        // e já atribuir valores as variaveis por meio dos metodos Setters:
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição do Curso Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("Descrição do Curso JS");
+        curso2.setCargaHoraria(6);
+
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria Java");
+        mentoria1.setDescricao("Descrição Mentoria Java");
+        mentoria1.setData(LocalDate.now()); // para que ao criar ele atribuirá a data deste
+        // momento de criação
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
     }
 }
